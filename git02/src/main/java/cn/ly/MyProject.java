99399b6edbb8827450a97628d566b72c8c7a4675
ly0607 <17457453ly>
2020-09-13 18:18:25 +0800

    jj

---
+++ git02/src/main/java/cn/ly/MyProject.java
@@ -0,0 +1,12 @@
+package cn.ly;
+
+public class MyProject {
+    public void say(){
+        System.out.println("hello");
+    }
+
+    public static void main(String[] args) {
+        MyProject myProject = new MyProject();
+        myProject.say();
+    }
+}
