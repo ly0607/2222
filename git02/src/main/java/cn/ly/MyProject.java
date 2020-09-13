2d402a3bed743dc2d36822221c7495add40998e9
ly0607 <17457453ly>
2020-09-13 18:21:00 +0800

    更新2

--- git02/src/main/java/cn/ly/MyProject.java
+++ git02/src/main/java/cn/ly/MyProject.java
@@ -5,8 +5,15 @@ public class MyProject {
         System.out.println("hello");
     }
 
+    public int data(){
+        int i =0;
+        System.out.println(++i);
+        return i;
+    }
+
     public static void main(String[] args) {
         MyProject myProject = new MyProject();
         myProject.say();
+        myProject.data();
     }
 }
